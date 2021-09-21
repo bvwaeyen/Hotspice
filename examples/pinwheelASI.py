2b3f61189a419933f9fbf4f440d58a8d7b0ff38c
Jonathan Maes <Jonathan.Maes@UGent.be>
2021-09-21 14:53:40 +0200

    Moved autocorrelation plot functions to separate file

--- examples/pinwheelASI.py
+++ examples/pinwheelASI.py
@@ -3,10 +3,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from context import hotspin
 from matplotlib import animation
 from numpy.core.numeric import Inf
 # from scipy import signal
+
+import examplefunctions
+from context import hotspin
 
 
 ## Parameters, meshgrid
@@ -135,68 +137,9 @@ def animate_temp_rise(mm, animate=1, speed=1000, T_step=0.000005, T_max=0.4):
     plt.show()
 
 
-def autocorrelation_dist_dependence(mm):
-    ''' Shows the full 2D autocorrelation, as well as the binned autocorrelation
-        as a function of distance. '''
-    corr, d, corr_length = mm.Autocorrelation_fast(20)
-    print("Correlation length:", corr_length)
-
-    fig = plt.figure(figsize=(10, 4))
-    fig.suptitle('T=%.2f' % mm.T, font={"size":12})
-    ax1 = fig.add_subplot(121)
-    ax1.plot(d, corr)
-    ax1.set_xlabel(r'Distance [a.u.]')
-    ax1.set_ylabel(r'Autocorrelation')
-    ax2 = fig.add_subplot(122)
-    h1 = ax2.imshow(mm.correlation, origin='lower', cmap='bone')
-    c2 = plt.colorbar(h1)
-    c2.set_label(r'Correlation', rotation=270, labelpad=15)
-    plt.gcf().tight_layout()
-    plt.show()
-
-
-def autocorrelation_temp_dependence(mm, N=31, M=50, L=500, T_min=0.1, T_max=0.4):
-    ''' Shows how the correlation distance depends on the temperature. 
-        @param N [int] (31): Number of temperature steps between <T_min> and <T_max>.
-        @param M [int] (50): How many times to do <L> switches at each temperature,
-            which are then averaged to get the correlation length at each temperature.
-        @param L [int] (500): Number of switches between each measurement of the
-            correlation length.
-    '''
-    # Calculate the correlation distance as a function of temperature
+def autocorrelation_temp_dependence(mm):
     mm.Initialize_m('random')
-    TT = np.linspace(T_min, T_max, N)
-    T_step = TT[1] - TT[0]
-    corr_length = np.empty((N, M))
-    niter = np.ones(N, dtype=int)*10*L
-    niter[0] = 40*L
-    for j, T in enumerate(TT):
-        print('Temperature step %d/%d (T = %.2f) ...' % (j+1, N, T))
-        mm.T = T
-        for _ in range(niter[j]): # Update for a while to ensure that the different temperatures are not very correlated
-            mm.Update()
-        for k in range(M):
-            corr, d, corr_length[j,k] = mm.Autocorrelation_fast(20)
-            if k < M - 1: # Always except the last step
-                for i in range(L):
-                    mm.Update()
-    corr_ll = np.mean(corr_length, axis=1)
-
-    # Draw a nice plot of all this
-    fig = plt.figure(figsize=(10,3))
-    ax1 = fig.add_subplot(121)
-    extent = [-0.5, corr_length.shape[1]-0.5, T_min-T_step/2, T_max+T_step/2] # Adding all these halves to place the pixels correctly
-    im1 = ax1.imshow(corr_length, origin='lower', interpolation='nearest', cmap='bone', extent=extent, aspect='auto')
-    c1 = plt.colorbar(im1) 
-    c1.set_label(r'Correlation length [a.u.]', rotation=270, labelpad=15)
-    ax1.set_xlabel('Step (x%d)' % L)
-    ax1.set_ylabel('Temperature [a.u.]')
-    ax2 = fig.add_subplot(122)
-    ax2.plot(TT, corr_ll)
-    ax2.set_xlabel('Temperature [a.u.]')
-    ax2.set_ylabel(r'$\langle$Correlation length$\rangle$ [a.u.]')
-    plt.gcf().tight_layout()
-    plt.show()
+    examplefunctions.autocorrelation_temp_dependence(mm, N=31, M=50, L=500, T_min=0.1, T_max=0.4)
 
 
 if __name__ == "__main__":
@@ -206,5 +149,5 @@ if __name__ == "__main__":
     # curieTemperature(mm)
     # animate_quenching(mm, animate=3, speed=50)
     # animate_temp_rise(mm, animate=3, speed=1000)
-    # autocorrelation_dist_dependence(mm)
-    # autocorrelation_temp_dependence(mm, N=31)
+    # examplefunctions.autocorrelation_dist_dependence(mm)
+    # autocorrelation_temp_dependence(mm)
