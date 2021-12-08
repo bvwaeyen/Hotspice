197e2af5ffa272f797d716db5e76a260e43c5c49
Jonathan Maes <Jonathan.Maes@UGent.be>
2021-12-08 11:46:15 +0100

    Sorted profiling output

--- profile.bat
+++ profile.bat
@@ -5,7 +5,7 @@ rem The cProfile output is saved in a file as "/profiling/cProfile_<name>_<times
 
 IF NOT EXIST "%~dp0\profiling" md "%~dp0\profiling"
 IF "%1" == "-m" (
-    python -m cProfile -m %2 > "%~dp0\profiling\cProfile_%2_%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%.txt"
+    python -m cProfile -s tottime -m %2 > "%~dp0\profiling\cProfile_%2_%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%.txt"
 ) ELSE (
-    python -m cProfile %1 > "%~dp0\profiling\cProfile_%~nx1_%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%.txt"
+    python -m cProfile -s tottime %1 > "%~dp0\profiling\cProfile_%~nx1_%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%.txt"
 )
