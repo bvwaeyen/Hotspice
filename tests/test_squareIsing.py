961fe730e6d309005e199de3bacd57a900388716
Jonathan Maes <Jonathan.Maes@UGent.be>
2022-03-21 12:18:56 +0100

    Split plotting and calculation in test_squareIsing

--- tests/test_squareIsing.py
+++ tests/test_squareIsing.py
@@ -1,4 +1,5 @@
 import math
+import os
 
 import cupy as cp
 import matplotlib.pyplot as plt
@@ -23,7 +24,7 @@ def get_m_theory(T_min, T_max):
     return m_theory_range, m_theory
 
 
-def test_magnetization(T_steps=30, N=1000, verbose=False, plot=True, reverse=False):
+def test_magnetization(T_steps=21, N=1000, verbose=False, plot=True, reverse=False):
     ''' Performs a sweep of the temperature in <T_steps> steps. At each step, <N> Magnets.update() calls are performed.
         @param reverse [bool] (False): if True, the temperature steps are in descending order, otherwise ascending.
     '''
@@ -37,53 +38,64 @@ def test_magnetization(T_steps=30, N=1000, verbose=False, plot=True, reverse=Fal
         mm.T = T*T_c
         for _ in range(N): mm.update()
         m_avg[i] = abs(mm.m_avg)
-
-    if plot:
-        hotspin.plottools.init_fonts()
-        plt.scatter(T_range, m_avg)
-        plt.plot(*get_m_theory(T_lim[0]-.005, T_lim[1]+.005), color='black')
-        plt.legend(['Hotspin', 'Theory'])
-        plt.show()
     
     data = pd.DataFrame({"T": T_range, "m_avg": m_avg})
+    if plot: test_magnetization_plot(data)
     return data
+
+def test_magnetization_plot(data: pd.DataFrame, save=False):
+    hotspin.plottools.init_fonts()
+    plt.scatter(data['T'], data['m_avg'])
+    plt.plot(*get_m_theory(T_lim[0]-.005, T_lim[1]+.005), color='black')
+    plt.legend(['Hotspin', 'Theory'])
+    plt.show()
 
 def test_N_influence(*args, plot=True, save=True, **kwargs):
     ''' Tests the influence of <N> (the number of iterations per value of T) on m_avg.
         Any arguments (not 'N') passed to this function are passed through to test_magnetization().
     '''
     data = pd.DataFrame()
-    if plot:
-        hotspin.plottools.init_fonts()
-        fig = plt.figure(figsize=(5, 3.5))
-        ax = fig.add_subplot(111)
     
     N_range = [1000, 2000, 4000]
     for N in N_range:
         local_data = test_magnetization(*args, N=N, plot=False, **kwargs)
         local_data["N"] = N
         data = pd.concat([data, local_data])
-        if plot: ax.scatter(local_data["T"], local_data["m_avg"])
 
     Tsweep_direction = 'reverse' if kwargs.get('reverse', False) else ''
-    savename = f"results/test_squareIsing/J={J/kB:.0f}kB_Tsweep{data['T'].nunique()}{Tsweep_direction}_Nsweep{len(N_range)}_a={a:.2g}_{size}x{size}"
-    if plot:
-        ax.plot(*get_m_theory(T_lim[0]-.005, T_lim[1]+.005), color='black')
-        ax.legend([f'N={N}' for N in N_range] + ['Theory'])
-        ax.set_xlabel('Temperature $T/T_c$')
-        ax.set_ylabel('Magnetization $\\langle M\\rangle /M_0$')
-        ax.set_xlim([T_lim[0]-.005, T_lim[1]+.005])
-        ax.set_ylim([-0.01, 1])
-        plt.gcf().tight_layout()
-        if save:
-            Tsweep_direction = 'reverse' if kwargs.get('reverse', False) else ''
-            hotspin.plottools.save_plot(f"{savename}.pdf")
-        plt.show()
-    if save:
-        hotspin.plottools.save_data(data, f"{savename}.csv")
+    savename = f"results/test_squareIsing/J={J/kB:.0f}kB_Tsweep{data['T'].nunique()}{Tsweep_direction}_Nsweep{data['N'].nunique()}_a={a:.2g}_{size}x{size}"
+    if plot: test_N_influence_plot(data, save=savename)
+    if save: hotspin.plottools.save_data(data, f"{savename}.csv")
     
     return data
 
+def test_N_influence_plot(data: pd.DataFrame, save=False):
+    ''' If <save> is bool, the filename is automatically generated. If <save> is str, it is used as filename. '''
+    hotspin.plottools.init_fonts()
+    fig = plt.figure(figsize=(5, 3.5))
+    ax = fig.add_subplot(111)
+    
+    T_lim = [data["T"].min(), data["T"].max()]
+    ascending = data["T"].iloc[0] < data["T"].iloc[-1]
+
+    for N, local_data in data.groupby("N"):
+        ax.scatter(local_data["T"], local_data["m_avg"], label=f'N={N}')
+
+    ax.plot(*get_m_theory(T_lim[0]-.005, T_lim[1]+.005), color='black', label='Theory')
+    ax.legend()
+    ax.set_xlabel('Temperature $T/T_c$')
+    ax.set_ylabel('Magnetization $\\langle M\\rangle /M_0$')
+    ax.set_xlim([T_lim[0]-.005, T_lim[1]+.005])
+    ax.set_ylim([-0.01, 1])
+    plt.gcf().tight_layout()
+    if save:
+        if type(save) is str:
+            savename = os.path.splitext(save)[0] + '.pdf'
+        else:
+            savename = f"results/test_squareIsing/Tsweep{data['T'].nunique()}{'' if ascending else 'reverse'}_Nsweep{data['N'].nunique()}.pdf"
+        hotspin.plottools.save_plot(f"{savename}.pdf")
+    plt.show()
+
 
 if __name__ == "__main__":
-    test_N_influence(T_steps=3, verbose=True, save=True)
+    test_N_influence(T_steps=1, verbose=True, save=True)
