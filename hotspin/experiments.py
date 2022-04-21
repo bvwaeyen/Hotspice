8e006a47bf3720fefc69c13e441f9185cfc03fd6
Jonathan Maes <Jonathan.Maes@UGent.be>
2022-04-21 17:30:46 +0200

    [WIP] Add first version of TaskAgnosticExperiment

--- hotspin/experiments.py
+++ hotspin/experiments.py
@@ -4,12 +4,13 @@ import warnings
 
 import cupy as cp
 import numpy as np
+import statsmodels.api as sm
 
 from abc import ABC, abstractmethod
-# from cupyx.scipy import signal
 
 from .core import Magnets, DipolarEnergy, ZeemanEnergy
 from .io import Inputter, OutputReader, RandomBinaryDatastream, FieldInputter, PerpFieldInputter, RandomUniformDatastream, RegionalOutputReader
+from .plottools import init_interactive, show_m, init_fonts
 from .utils import strided, shell
 
 
@@ -58,6 +59,151 @@ class KernelQualityExperiment(Experiment):
         return self.results['rank']
 
 
+class TaskAgnosticExperiment(Experiment):
+    def __init__(self, inputter, outputreader, mm):
+        ''' Follows the paper
+                J. Love, J. Mulkers, G. Bourianoff, J. Leliaert, and K. Everschor-Sitte. Task agnostic
+                metrics for reservoir computing. arXiv preprint arXiv:2108.01512, 2021.
+            to implement task-agnostic metrics for reservoir computing using a single random input signal.
+        '''
+        super().__init__(inputter, outputreader, mm)
+        self.k = None
+
+    def run(self, N=1000, k=10, verbose=False):
+        ''' @param N [int]: The total number of iterations performed (each iteration consists of <self.inputter.n> MC steps).
+            @param k [int]: The number of iterations back in time that are used to train the current time step.
+        '''
+        if N <= k: raise ValueError(f"Number of iterations N={N} must be larger than k={k}.")
+        self.k = k
+        self.mm.relax()
+        if verbose:
+            init_fonts()
+            init_interactive()
+        # First, we run the simulation for <N> steps where each step consists of <inputter.n> full Monte Carlo steps.
+        self.u = cp.zeros(N) # Inputs
+        self.y = cp.zeros((N, self.outputreader.n)) # Outputs
+        fig = None
+        print(f'[0/{N}] Running TaskAgnosticExperiment...')
+        for i in range(N):
+            self.u[i] = self.inputter.input_single(self.mm)
+            self.y[i,:] = self.outputreader.read_state().reshape(-1)
+            if verbose:
+                # print(self.u[i])
+                if (i + 1) % 10**(math.floor(math.log10(N))-1) == 0 or i == 0: 
+                    print(f'[{i+1}/{N}] {self.mm.switches}/{self.mm.attempted_switches} switching attempts successful ({self.mm.MCsteps:.2f} MC steps).')
+                fig = show_m(self.mm, figure=fig)
+        # Then, we use the recorded information to perform some funni calculations
+        self.results['NL'] = self.NL(k=k, verbose=verbose)
+        self.results['MC'] = self.MC(k=k)
+        self.results['CP'] = self.CP()
+        self.results['S'] = self.S()
+    
+    def NL(self, k=None, local=False, test_fraction=1/4, verbose=False): # Non-linearity
+        ''' Returns a float (local=False) or a CuPy array (local=True) representing the nonlinearity,
+            either globally or locally depending on <local>. For this, an estimator for the current readout state is
+            trained which for any time instant has access to the <k> most recent inputs (including the present one).
+        '''
+        if k is None: k = 10 if self.k is None else self.k
+        if self.u.size <= k: raise ValueError("Number of iterations must be larger than k.")
+        if self.u.size < k + 10: warnings.warn(f"k={k} might be tiny touch too big to get a good train/test split.", stacklevel=2) # Just a wet finger estimate
+
+        train_test_cutoff = math.ceil(self.u.size*(1-test_fraction))
+        u_train, u_test = cp.split(self.u, [train_test_cutoff])
+        y_train, y_test = cp.split(self.y, [train_test_cutoff]) # Need to put train_test_cutoff in iterable for correct behavior
+
+
+        u_strided = strided(self.u, k) # List of last <k> entries in <u> for each time step (result: N x k array)
+        u_train_strided, u_test_strided = np.split(u_strided, [train_test_cutoff])
+        # u_train_strided, y_train = u_train_strided[k-1:], y_train[k-1:] # First <k-1> rows don't have <k> previous entries yet to use as 'samples'
+        Rsq = cp.empty(self.outputreader.n) # R² correlation coefficient
+        for j in range(self.outputreader.n): # Train an estimator for the j-th output feature
+            model = sm.OLS(y_train[:,j].get(), u_train_strided.get(), missing='drop') # missing='drop' ignores samples with NaNs (i.e. the first k-1 samples)
+            results = model.fit()
+            y_hat_test = results.predict(u_test_strided.get())
+            sigma_y_hat, sigma_y = np.var(y_hat_test), np.var(y_test[:,j])
+            if sigma_y_hat == 0: # (highly unlikely) predicting constant value, so R² depends on whether this constant is the average or not
+                Rsq[j] = float(cp.mean(y_hat_test) == cp.mean(y_test[:,j]))
+            elif sigma_y == 0: # Constant readout, so NL should logically be 0, so Rsq = 1-NL = 1
+                Rsq[j] = 1.
+            else: # Rsq is 1 for very predictable 
+                Rsq[j] = (np.cov(y_hat_test, y_test[:,j], bias=True)[0, 1]**2)/(sigma_y_hat*sigma_y) # Same as np.corrcoef(y_hat_test, y_test[:,j])[0,1]**2
+                # Rsq[j] = (np.cov(y_hat_test, y_test[:,j], bias=True)[0, 1])/(np.std(y_hat_test)*np.std(y_test[:,j])) # Same as np.corrcoef(y_hat_test, y_test[:,j])[0,1]
+            if verbose:
+                print(f'----- NONLINEARITY OF NODE {j} -----')
+                # print(y_hat_test, y_test[:,j])
+                print('cov std std:', np.cov(y_hat_test, y_test[:,j], bias=True)[0, 1], np.std(y_hat_test), np.std(y_test[:,j]))
+                print('product:', (np.cov(y_hat_test, y_test[:,j], bias=True)[0, 1])/(np.std(y_hat_test)*np.std(y_test[:,j])))
+                print('Rsq (using cov² and var):', Rsq[j])
+
+        # Handle <local> True or False and return appropriately
+        if local:
+            return 1 - Rsq
+        else:
+            return float(1 - cp.mean(Rsq))
+    
+    def MC(self, k=None, local=False, test_fraction=1/4): # Memory capacity
+        ''' Returns a float (local=False) or a CuPy array (local=True) representing the memory capacity,
+            either globally or locally depending on <local>. For this, an estimator is trained which
+            for any time instant attempts to predict the previous <k> inputs based on the current readout state.
+        '''
+        if k is None: k = 10 if self.k is None else self.k
+        if self.u.size <= k: raise ValueError("Number of iterations must be larger than k.")
+        # if self.u.size < k + 10: warnings.warn(f"k={k} might be tiny touch too big to get a good train/test split.", stacklevel=2) # Just a wet finger estimate
+
+        train_test_cutoff = math.ceil(self.u.size*(1-test_fraction))
+        u_train, u_test = cp.split(self.u, [train_test_cutoff])
+        y_train, y_test = cp.split(self.y, [train_test_cutoff]) # Need to put train_test_cutoff in iterable for correct behavior
+
+        if u_test.size < k: warnings.warn(f"k={k} is too large for the currently stored results (size {self.u.size}), possibly causing issues with train/test split.", stacklevel=2)
+        Rsq = cp.empty(k) # R² correlation coefficient
+        weights = cp.zeros((k, self.outputreader.n))
+        for j in range(1, k+1): # Train an estimator for the input j iterations ago
+            # This one is a mess of indices, but at least we don't need striding like for non-linearity
+            results = sm.OLS(u_train[k-j:-j].get(), y_train[k:,:].get(), missing='drop').fit() # missing='drop' ignores samples with NaNs (i.e. the first k-1 samples)
+            weights[j-1,:] = cp.asarray(results.params)
+            u_hat_test = results.predict(y_test[j:,:].get()) # Start at y_test[j] to prevent leakage between train/test
+            Rsq[j-1] = (np.cov(u_hat_test, u_test[:-j], bias=True)[0, 1]**2)/(np.var(u_hat_test)*np.var(u_test[:-j])) # Same as np.corrcoef(u_hat_test, u_test[:,-j])[0,1]**2
+        Rsq[cp.isnan(Rsq)] = 0 # If some std=0, then it is constant so R² actually gives 0
+
+        # Handle <local> True or False and return appropriately
+        if local:
+            # TODO: I think some normalization is wrong here (result is all near 0.5, not full range between 0 and 1 as in paper)
+            weights = weights - cp.min(weights, axis=1).reshape(-1, 1) # Minimum value becomes 0
+            weights = weights / cp.max(weights, axis=1).reshape(-1, 1) # Maximum value becomes 1
+            weights_avg = cp.mean(weights, axis=0) # Average across 'time'
+            return weights_avg
+        else:
+            return float(cp.sum(Rsq))
+
+    def CP(self, averaged=True): # Complexity
+        ''' Calculates the complexity: i.e. the effective rank of the kernel matrix as used in KernelQualityExperiment.
+            If averaged is True, the complexity is averaged over all possible square matrices to be found in <self.y>,
+            if averaged is False, the complexity is calculated using the last <self.outputreader.n> entries in <self.y>.
+        '''
+        if self.y.shape[0] < self.y.shape[1]: # Less rows than columns, so rank can at most be the number of rows
+            warnings.warn(f"Not enough recorded iterations to reliably calculate complexity (shape {self.y.shape} has less rows than columns)." , stacklevel=2)
+        
+        if averaged:
+            ranks = cp.asarray([cp.linalg.matrix_rank(self.y[i:i+self.y.shape[1],:]) for i in range(max(1, self.y.shape[0]-self.y.shape[1]+1))])
+            rank = cp.mean(ranks)
+        else:
+            # Multiply with transposed to get a square matrix of size <self.outputreader.n> (suggested by J. Leliaert)
+            squarematrix = cp.matmul(self.y.T, self.y)
+            rank = cp.linalg.matrix_rank(squarematrix)
+        return int(rank)/self.outputreader.n
+        # TODO: observations show that averaged=False gives a bit higher values than True, but which is better?
+    
+    def S(self): # Stability
+        ''' Calculates the stability: i.e. how similar the initial and final states are, after relaxation.
+            NOTE: This function is not normalized.
+            NOTE: It can be advantageous to define a different metric for stability if the entire magnetization profile
+                  is known, since this function only has access to the readout nodes, not the whole self.m array.
+        '''
+        self.mm.relax()
+        final_readout = self.outputreader.read_state().reshape(-1)
+        initial_readout = self.y[0,:] # Assuming this was relaxed first
+        return float(cp.sum((initial_readout - final_readout)**2))
+
 ###############################################################################
 def main_kernelquality():
     from .ASI import PinwheelASI
@@ -83,5 +229,16 @@ def main_kernelquality():
     plt.savefig(f'{os.path.splitext(filename)[0]}.pdf')
     plt.show()
 
+def main_taskagnostic():
+    from .ASI import PinwheelASI
+    mm = PinwheelASI(25, 1e-6, T=100, energies=(DipolarEnergy(), ZeemanEnergy()), PBC=False, pattern='vortex')
+    datastream = RandomUniformDatastream(low=-1, high=1)
+    inputter = FieldInputter(datastream, magnitude=3e-5, angle=math.pi/180*7, n=2)
+    outputreader = RegionalOutputReader(2, 2, mm)
+    experiment = TaskAgnosticExperiment(inputter, outputreader, mm)
+    experiment.run(N=100, verbose=True)
+    print(experiment.results)
+    shell()
 if __name__ == "__main__": # Run this file from the parent directory using cmd 'python -m hotspin.experiments'
-    main_kernelquality()
+    # main_kernelquality()
+    main_taskagnostic()
