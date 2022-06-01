bd91ec4d9a9d9250bcfe8ec3e984300256df85dd
Jonathan Maes <Jonathan.Maes@UGent.be>
2022-06-01 16:44:51 +0200

    E_landscape_sinusoidal.py prints different E_b estimates

--- analysis/E_landscape_sinusoidal.py
+++ analysis/E_landscape_sinusoidal.py
@@ -7,18 +7,29 @@ def E(E_B, ext_field, angle):
     return lambda x: -E_B*(np.cos(2*x)-1)/2 - ext_field*(np.cos(x - angle)-1)/2
 
 
-def plot(E_B, ext_field, angle):
+def plot(E_B, ext_field, angle, verbose=True):
     if ext_field < 0: ext_field, angle = -ext_field, angle + np.pi
     angle = angle % (2*np.pi)
 
-    x = np.linspace(0, 2*np.pi, 200)
-    y = E(E_B, ext_field, angle)(x)
+    E_func = E(E_B, ext_field, angle)
+    x = np.linspace(0, 2*np.pi, 1024+1)
+    y = E_func(x)
     maxima = np.where((y > np.roll(y, 1)) & (y > np.roll(y, -1)))
     minima = np.where((y < np.roll(y, 1)) & (y < np.roll(y, -1)))
 
     p,  = plt.plot(x, y, label=f"{E_B} + {ext_field} ({angle*180/np.pi:.0f}°)")
     plt.scatter(np.concatenate([x[maxima], x[minima]]), np.concatenate([y[maxima], y[minima]]), color=p.get_color())
     plt.axvline(angle, linewidth=1, color=p.get_color())
+
+    if verbose:
+        print(f"For external field at {angle*180/np.pi:.0f}°:")
+        # The most accurate measure: using the analytical minima and maxima to calculate the effective energy barrier
+        print(u"∞" + f"-state E_b = {np.min(y[maxima]) - np.max(y[minima]):.2e} J")
+        # An accurate discrete measure: assume that maxima and minima occur at 0°, 90°, 180°, 270° exactly
+        print(f"4-state E_b = {min(E_func(np.pi/2), E_func(-np.pi/2)) - max(E_func(0), E_func(np.pi)):.2e} J") 
+        # Inaccurate, but the best if one only has access to the two stable states: E_b = E_B + (E(0°)-E(180°))/2
+        print(f"2-state E_b = {max((delta_E := -abs(E_func(0) - E_func(np.pi))), E_B + delta_E/2):.2e} J")
+    return E_func
 
 
 def show():
@@ -35,6 +46,8 @@ def show():
 
 if __name__ == "__main__":
     # Example for pinwheel ASI sublattices in a global 30° external field (cfr. pinwheel reversal test)
-    plot(1, -1, -np.pi/2 + np.pi/12)
-    plot(1, -1, np.pi/12)
+    E_B, ext_field, ext_angle = 1, -1, -np.pi/6
+    E1 = plot(E_B, ext_field, -np.pi/4 + ext_angle)
+    E2 = plot(E_B, ext_field, np.pi/4 + ext_angle)
+
     show()
